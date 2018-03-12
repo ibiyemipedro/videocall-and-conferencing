219216a3ab097d12cbd7f621b020df6083e6bb28
Ibiyemi <yemipedro07>
2018-03-12 20:35:13 +0100

    first commit

---
+++ js/video.js
@@ -0,0 +1,24 @@
+(function () {
+	var video = document.getElementById('video'),
+		vendorUrl = window.URL || window.webkitURL;
+
+	navigator.getMedia = 	navigator.getUserMedia ||
+							navigator.webkitGetUserMedia ||
+							navigator.mozGetUserMedia ||
+							navigator.msGetUserMedia;
+
+	// capture video
+	navigator.getMedia({
+		video: true,
+		audio: false
+
+	}, function(stream) {
+		video.src = vendorUrl.createObjectURL(stream);
+		video.play();
+	}, function(error) {
+		// An error occured
+		// error.code
+
+	});
+
+})();
